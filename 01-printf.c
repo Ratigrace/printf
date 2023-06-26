b93d5e69b5f42d9800ee3ecaf60dc7b5aaf53c37
KLEINBOOI MATHAILA <phaswanemathaila@gmail.com>
2023-06-26 13:59:35 -0700

    a function prints output according to format

--- 01-printf.c
+++ 01-printf.c
@@ -4,47 +4,178 @@
 #include <unistd.h>
 #include <stdio.h>
 
+#define BUFF_SIZE 1024
+
 /**
- * _printf - produces output according to a format
+ * print_buffer - Prints the contents of the buffer if it exists
  *
- * @format: is a charecter string
+ * @buffer: Array of characters containing the buffered output
+ * @buff_ind: Pointer to the index at which to add the next character
  *
- * Return: 0 always
+ * Return: prints the contents of the buffer and resets the index.
  */
 
+void print_buffer(char buffer[], int *buff_ind);
+
+
+/**
+ * get_width - Extracts the width value from the format specifier
+ *
+ * @format: Format string
+ * @index: Pointer to the current position in the format string
+ *
+ * Return: The extracted width value
+ */
+
+int get_width(const char *format, int *index)
+{
+    int width = 0;
+
+    while (format[*index] >= '0' && format[*index] <= '9')
+    {
+        width = width * 10 + (format[*index] - '0');
+        (*index)++;
+    }
+
+    return width;
+}
+
+/**
+ * handle_print - Handles the printing of characters based on the format
+ *		specifier
+ *
+ * @format: Format string
+ * @index: Pointer to the current position in the format string
+ * @list: va_list of arguments
+ * @buffer: Array of characters for buffering output
+ * @buff_ind: Pointer to the index at which to add the next character
+ * @width: Width value for formatting
+ *
+ * Return: number of characters printed
+ */
+
+int handle_print(const char *format, int *index, va_list list, char buffer[], int *buff_ind, int width)
+{
+    char c;
+    char *str;
+    int printed_chars = 0, i;
+
+    if (format[*index] == 'c')
+    {
+        c = (char)va_arg(list, int);
+        buffer[*buff_ind] = c;
+        (*buff_ind)++;
+        if (*buff_ind == BUFF_SIZE)
+        {
+            print_buffer(buffer, buff_ind);
+            printed_chars += BUFF_SIZE;
+        }
+        (*index)++;
+    }
+    else if (format[*index] == 's')
+    {
+        str = va_arg(list, char *);
+        i = 0;
+        while (str[i] != '\0')
+        {
+            buffer[*buff_ind] = str[i];
+            (*buff_ind)++;
+            if (*buff_ind == BUFF_SIZE)
+            {
+                print_buffer(buffer, buff_ind);
+                printed_chars += BUFF_SIZE;
+            }
+            i++;
+        }
+        (*index)++;
+    }
+    else if (format[*index] == '%')
+    {
+        buffer[*buff_ind] = '%';
+        (*buff_ind)++;
+        if (*buff_ind == BUFF_SIZE)
+        {
+            print_buffer(buffer, buff_ind);
+            printed_chars += BUFF_SIZE;
+        }
+        (*index)++;
+    }
+    else
+    {
+        return -1;
+    }
+	width++;
+    return printed_chars;
+}
+
+/**
+ * _printf - Produces output according to a format
+ *
+ * @format: Character string specifying the format
+ *
+ * Return: The number of characters printed
+ *
+ */
 int _printf(const char *format, ...)
 {
-	int z, len;
-	char ch;
-	char *str;
+	int i, printed_chars = 0;
+	int buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	va_list list_arg;
+	if (format == NULL)
+		return -1;
 
-	va_start(list_arg, format);
+	va_start(list, format);
 
-	for (z = 0; format[z] != '\0'; z++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[z] == '%' && (format[z + 1] == 'c'))
+		if (format[i] != '%')
 		{
-			ch = (char)va_arg(list_arg, int);
-			write(1, &ch, 1);
-			format += 2;
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+			{
+				print_buffer(buffer, &buff_ind);
+				printed_chars += buff_ind;
+			}
 		}
-		if (format[z] == '%' && format[z + 1] == 's')
+		else
 		{
-			str = va_arg(list_arg, char*);
-			len = _strlen(str);
-			write(1, str, len);
-			format += 2;
-		}
-		if (format[z] == '%' && format[z + 1] == '%')
-		{
-			write(1, "%", 1);
-			format += 2;
-		}
-		write(1, &format[z], 1);
-		}
+			i++;
 
-		va_end(list_arg);
-		return (0);
+			if (format[i] == '%')
+			{
+				buffer[buff_ind++] = '%';
+				if (buff_ind == BUFF_SIZE)
+				{
+					print_buffer(buffer, &buff_ind);
+					printed_chars += buff_ind;
+				}
+			}
+			else
+			{
+				int width = get_width(format, &i);
+				int printed = handle_print(format, &i, list, buffer, &buff_ind, width);
+				if (printed == -1)
+					return -1;
+				printed_chars += printed;
+			}
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+	printed_chars += buff_ind;
+
+	va_end(list);
+
+	return printed_chars;
+}
+
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+	{
+		write(1, buffer, *buff_ind);
+		*buff_ind = 0;
+	}
 }
