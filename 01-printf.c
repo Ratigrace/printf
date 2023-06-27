8378fcc5ebc3cc8ca0b91a8b41a69741319aae86
KLEINBOOI MATHAILA <phaswanemathaila@gmail.com>
2023-06-27 13:05:17 -0700

    Updated the comments

--- 01-printf.c
+++ 01-printf.c
@@ -2,46 +2,53 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * _printf - produces output according to a format
+ *
+ * @format: is a character string
+ *
+ * Return: the number of characters printed
+ */
 
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	int printed_chars = 0;
-	va_list args;
+	int count = 0, number_of_chars = 0;
+	va_list list_arg;
 
-    va_start(args, format);
-    
-    while (*format) {
-        if (*format != '%') {
-            putchar(*format);
-            printed_chars++;
-        } else {
-            format++;
+	va_start(list_arg, format);
 
-            switch (*format) {
-                case 'c':
-                    count = printf("%c", va_arg(args, int));
-                    break;
-                case 's':
-                    count = printf("%s", va_arg(args, char *));
-                    break;
-                case 'd':
-                case 'i':
-                    count = printf("%d", va_arg(args, int));
-                    break;
-                default:
-                    putchar('%');
-                    putchar(*format);
-                    count = 2;
-                    break;
-            }
-
-            printed_chars += count;
-        }
-
-        format++;
-    }
-
-    va_end(args);
-    return printed_chars;
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			_putchar(*format);
+			number_of_chars++;
+		}
+		else
+		{
+			format++;
+			switch (*format)
+			{
+			case 'c':
+				count = printf("%c", va_arg(list_arg, int));
+				break;
+			case 's':
+				count = printf("%s", va_arg(list_arg, char *));
+				break;
+			case 'd':
+			case 'i':
+				count = printf("%d", va_arg(list_arg, int));
+				break;
+			default:
+				_putchar('%');
+				_putchar(*format);
+				count = 2;
+				break;
+			}
+			number_of_chars += count;
+		}
+		format++;
+	}
+	va_end(list_arg);
+	return (number_of_chars);
 }
