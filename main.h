a08b6f9c4fe2da6fe1bd91921a1097fd0e33978a
Ratigrace <chijongwegrace@gmail.com>
2023-06-27 14:35:23 -0700

    updated main.h

--- main.h
+++ main.h
@@ -4,5 +4,10 @@
 int main(void);
 int _printf(const char *format, ...);
 int _putchar(char c);
+int _strlen(const char *str);
+int _putstr(const char *str);
+int _putint(int n);
+void _strrev(char *str);
+
 
 #endif
